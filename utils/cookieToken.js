d6a6c5c33d8d5ce13dde94820c363b7b3604bf45
Aman123at <ammitiwari1234@gmail.com>
2023-12-11 19:59:19 +0530

    changes in cookie token

--- utils/cookieToken.js
+++ utils/cookieToken.js
@@ -5,7 +5,8 @@ const cookieToken = (user,res)=>{
         expires : new Date(
             Date.now() + (process.env.COOKIE_TIME*24*60*60*1000)
         ),
-        httpOnly:true
+        httpOnly:false,
+        
 
     }
     user.password = undefined
