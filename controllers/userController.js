0a2f028b42b4564d1a1087d17c2af459fc52c4f4
Aman123at <ammitiwari1234@gmail.com>
2023-12-13 21:52:24 +0530

    changed cookies setting in logout

--- controllers/userController.js
+++ controllers/userController.js
@@ -104,7 +104,9 @@ exports.login = async (req,res,next)=>{
 exports.logout = async (req,res,next)=>{
     res.cookie('token',null,{
         expires:new Date(Date.now()),
-        httpOnly:true
+        httpOnly:true,
+        sameSite: 'None',
+        secure: true,
     })
     res.status(200).json({
         success:true,
